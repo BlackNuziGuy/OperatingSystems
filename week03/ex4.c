82a5fd5261e813ffcb49b9e662757d09eed606a1
Vitaliy <42554566+BlackNuziGuy@users.noreply.github.com>
2019-09-05 20:11:29 +0300

    Added ex4

---
+++ week03/ex4.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high- 1; j++)
+    {
+
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = partition(arr, low, high);
+
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    quickSort(arr, 0, 6);
+    for (int i=0; i < 6; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+} 
