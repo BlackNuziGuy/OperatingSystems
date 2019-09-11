701938512c1d37f268ba950bd56b6648f92d4f1c
Vitaliy <vitalykorbashov@gmail.com>
2019-09-11 16:10:00 +0300

    Update ex1.c

--- week04/ex1.c
+++ week04/ex1.c
@@ -5,12 +5,14 @@
 
 int main(void) {
     
-    int pid, n = 228;
+    int n;
     
-    if (pid = fork() > 0) {
-        printf("Hello from parent [%d - %d]\n", pid, n);
+    int pid = fork();
+    
+    if ( pid > 0) {
+        printf("Hello from parent [%d - %d]\n", getpid(), n);
     } else if (pid == 0) {
-        printf("Hello from child [%d - %d]\n", pid, n);
+        printf("Hello from child [%d - %d]\n", getpid(), n);
     }
      
     return 0;
