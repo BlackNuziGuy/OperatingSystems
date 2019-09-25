9e05b461685605375494da8cd75a8afda2744c2e
Vitaliy <vitalykorbashov@gmail.com>
2019-09-25 17:40:26 +0300

    Added ex2

---
+++ week06/ex2.c
@@ -0,0 +1,35 @@
+#include <stdio.h> 
+#include <unistd.h> 
+#include <stdlib.h>
+#define MSGSIZE 13 
+
+
+
+int main() 
+{ 
+	char* msg = "hello, pepe";  
+
+	//setup a pipe
+	int p[2], i; 
+	if (pipe(p) < 0) 
+		exit(1); 
+
+
+	//concept a child
+	int pid  = fork();
+
+
+	//parent
+	if (pid == 0) write(p[1],msg, MSGSIZE);
+	
+	// child
+	else{
+		char dest[MSGSIZE];
+
+		read(p[0],dest, MSGSIZE);
+		printf("% s\n", dest); 
+	}
+    
+    
+	return 0; 
+} 
