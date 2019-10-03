32a709088ff1a3b24f1241f3cda534e7ea07ae8f
Vitaliy <42554566+BlackNuziGuy@users.noreply.github.com>
2019-10-03 10:42:40 +0300

    Update ex5.c

--- week07/ex5.c
+++ week07/ex5.c
@@ -3,15 +3,15 @@
 
 int main() {
 
-	 //Just so there is actually a reason to have a 2d array
+	 //Is there actually a reason to have a 2d array?
 	char **s = (char**) malloc(sizeof(char*));
 	char foo[] = "Hello World";
 
 	*s = foo;
-	printf("s is %s\n", *s);	
+	printf("The only content of s is *s = %s\n", *s);	
 	
 
-	s[0] = foo;
+	s[0] = foo; // the same as *s = foo
 	printf("s[0] is %s\n",s[0]);
 	
 	return 0;
